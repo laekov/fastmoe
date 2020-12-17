302835707fd29dbaab7f95c0f39ce3d10ef2fb97
Rick Ho <laekov.h@gmail.com>
2020-12-17 17:14:07 +0800

    add timer

---
+++ pytorch/cuda/timer.hh
@@ -0,0 +1,16 @@
+#ifndef TIMER_HH
+#define TIMER_HH
+
+#include <chrono>
+
+inline double getDuration(std::chrono::time_point<std::chrono::system_clock> a,
+		                    std::chrono::time_point<std::chrono::system_clock> b) {
+	      return  std::chrono::duration<double>(b - a).count();
+}
+
+#define timestamp(__var__) auto __var__ = std::chrono::system_clock::now();
+
+#include <chrono>
+
+#endif  // TIMER_HH
+
