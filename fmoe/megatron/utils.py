c3f3579568b50224dd2ba9c23652a50da6696263
xptree <jiezhongqiu@outlook.com>
2023-07-04 01:30:37 +0800

    keep num_experts for lower version of megatron

--- fmoe/megatron/utils.py
+++ fmoe/megatron/utils.py
@@ -1,11 +1,17 @@
 r"""
 Utility in Megatron
 """
+
+import argparse
+
 def add_fmoe_args(parser):
     group = parser.add_argument_group(title="fastmoe")
 
     group.add_argument("--fmoefy", action="store_true")
-    group.add_argument("--fmoe-num-experts", type=int, default=None)
+    try:
+        group.add_argument("--num-experts", type=int, default=None)
+    except argparse.ArgumentError:
+        group.add_argument("--fmoe-num-experts", type=int, default=None)
     group.add_argument("--top-k", type=int, default=2)
     group.add_argument("--balance-loss-weight", type=float, default=1)
     group.add_argument("--balance-strategy", type=str, default=None)
